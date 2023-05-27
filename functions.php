45deddb2a94afd4bd6356e01720b0577b677ebeb
losbenos <losbenos@me.com>
2023-05-27 08:42:21 +0100

    Block variation, style + pattern category examples

--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php
 
+define( 'CHIMERA_THEME_VERSION', wp_get_theme()->get( 'Version' ) );
+
+
+
+
 if ( ! function_exists( 'chimera_master_support' ) ) :
 	function chimera_master_support()  {
 
@@ -12,31 +17,86 @@ if ( ! function_exists( 'chimera_master_support' ) ) :
 		//remove core block patterns
 		remove_theme_support( 'core-block-patterns' );
 
+		//add support for classinc menus
+		add_theme_support( 'menus' );
+
+		//reinstate the customizer (useful for some woo setings)
+		//add_action( 'customize_register', '__return_true' );
 	}
 	add_action( 'after_setup_theme', 'chimera_master_support' );
 endif;
-//remove pattern directory patterns (not needed if we've used remove_theme_support( 'core-block-patterns' ))
-//add_filter( 'should_load_remote_block_patterns', '__return_false' );
 
 
-/**
- * Enqueue scripts and styles.
- */
+
+
+
+//Enqueue scripts and styles.
 function chimera_master_scripts() {
 	// Enqueue theme stylesheet.
-	wp_enqueue_style( 'chimera-master-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_register_style( 'chimera-master-style', get_template_directory_uri() . '/style.css', array(), CHIMERA_THEME_VERSION );
+	wp_enqueue_style( 'chimera-master-style' );
 }
-
 add_action( 'wp_enqueue_scripts', 'chimera_master_scripts' );
 
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
- */
+
+
+
+
+//Registers the block using the metadata loaded from the `block.json` file.
+//Behind the scenes, it also registers all assets so they can be enqueued
+//through the block editor in the corresponding context.
+
+//@see https://developer.wordpress.org/reference/functions/register_block_type/
 function chimera_custom_block_block_init() {
 	register_block_type( __DIR__ . '/custom-block/build' );
 }
 add_action( 'init', 'chimera_custom_block_block_init' );
+
+
+
+
+//Block style examples.
+require_once get_theme_file_path( 'inc/register-block-styles.php' );
+
+
+
+
+//This is an example of how to register a block variation.
+//Type /full or use the block inserter to insert a full width group block.
+
+//@see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-variations/
+function chimera_register_block_variation() {
+	wp_enqueue_script(
+		'chimera-block-variations',
+		get_template_directory_uri() . '/assets/js/block-variation.js',
+		array( 'wp-blocks' ),
+		CHIMERA_THEME_VERSION,
+		true
+	);
+}
+add_action( 'enqueue_block_editor_assets', 'chimera_register_block_variation' );
+
+
+
+
+
+
+//Registers block pattern categories and types.
+function chimera_register_block_pattern_categories() {
+
+	$block_pattern_categories = array(
+		'chimera-footer'       => array(
+			'label'         => __( 'Chimera Footer', 'chimera' ),
+			'categoryTypes' => array( 'chimera' ),
+		),
+		'chimera-general'      => array(
+			'label'         => __( 'Chimera General', 'chimera' ),
+			'categoryTypes' => array( 'chimera' ),
+		),
+	);
+
+	foreach ( $block_pattern_categories as $name => $properties ) {
+		register_block_pattern_category( $name, $properties );
+	}
+}
+add_action( 'init', 'chimera_register_block_pattern_categories', 9 );
---
+++ inc/register-block-styles.php
@@ -0,0 +1,32 @@
+<?php 
+
+/**
+ * Register block styles.
+ *
+ * @since 1.0
+ */
+function chimera_register_block_styles() {
+
+	$block_styles = array(
+		'core/button'         => array(
+			'shadow' => __( 'Shadow', 'chimera' ),
+		),
+		'core/group'           => array(
+			'grey'       => __( 'Grey', 'chimera' ),
+			'shadow' => __( 'Shadow', 'chimera' ),
+		),
+	);
+
+	foreach ( $block_styles as $block => $styles ) {
+		foreach ( $styles as $style_name => $style_label ) {
+			register_block_style(
+				$block,
+				array(
+					'name'  => $style_name,
+					'label' => $style_label,
+				)
+			);
+		}
+	}
+}
+add_action( 'init', 'chimera_register_block_styles' );
