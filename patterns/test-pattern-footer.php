bce416cc3ec70453fd8e3c60cf3a820b05e2481d
losbenos <losbenos@me.com>
2023-05-28 21:38:13 +0100

    Additional files added from previous commit

---
+++ patterns/test-pattern-footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Test Pattern.
+ * Slug: chimera/test-pattern-footer
+ * Description: This is just a test
+ * Categories: chimera-footer
+ * Keywords: 
+ * Viewport Width: 1200
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">This is a test pattern for the footer</h2>
+<!-- /wp:heading -->
---
+++ patterns/test-pattern-general.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Test Pattern.
+ * Slug: chimera/test-pattern
+ * Description: This is just a test
+ * Categories: chimera-general
+ * Keywords: 
+ * Viewport Width: 1200
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">This is a test pattern</h2>
+<!-- /wp:heading -->
